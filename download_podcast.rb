75658831329216f534011c8b5770d2f922bc4735
byplayer <byplayer100@gmail.com>
2020-04-23 00:13:42 +0900

    :+1: normalize file name
    The one or two digit is %02d format, but others are %03d format. So I
    normalize file name to %03d.

--- download_podcast.rb
+++ download_podcast.rb
@@ -15,14 +15,45 @@ response = agent.get(TARGET_URL)
 
 doc = Nokogiri::XML.parse(response.body)
 (doc / '/rss/channel/item').each do |item|
-  url = (item / 'guid').inner_text
-  file_name = File.basename(url, '*.*')
-  puts "download: #{file_name}"
-  file_path = File.join(DEST_DIR, file_name)
-  if File.exist?(file_path)
-    puts 'skip due to already exist'
-    next
+  enclosure = (item / 'enclosure')
+  url = enclosure.attr('url')
+  file_size = enclosure.attr('length').value.to_i
+  org_file_name = File.basename(url, '*.*')
+  unless org_file_name =~ /^(suzuki_vol)([0-9]+)(.*)/
+    puts 'file name format error'
+    break
   end
 
-  agent.download(url, file_path)
+  # normalize 3 degit
+  file_name =
+    format('%s%03d%s',
+           Regexp.last_match[1],
+           Regexp.last_match[2].to_i,
+           Regexp.last_match[3])
+
+  org_file_path = File.join(DEST_DIR, org_file_name)
+
+  print "download: #{file_name} "
+  file_path = File.join(DEST_DIR, file_name)
+
+  if org_file_path != file_path && File.exist?(org_file_path)
+    FileUtils.mv(org_file_path, file_path)
+  end
+
+  if File.exist?(file_path)
+    puts 'skip due to already exist'
+  else
+    puts
+    agent.download(url, file_path)
+  end
+
+  # verify size
+  # comment out this logic
+  # because some file isn't set correct mp3 file size.
+  # actual_size = File::Stat.new(file_path).size
+  # next unless actual_size != file_size
+
+  # puts "error: wrong file size(#{file_path})"
+  # puts "  expected: #{file_size}"
+  # puts "  actual  : #{actual_size}"
 end
